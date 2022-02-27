520b5c48a7cffda0dabdface4ec73985dec8ffce
oshishi <zyank.wpo.zyank.wpo@gmail.com>
2022-02-27 15:00:27 +0900

    5-4

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,29 @@
 import { useEffect } from 'react';
+import styled from 'styled-components';
 
 import { fetchRestaurants } from '../apis/restaurants';
+
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+
+const HeaderWrapper = styled.div`
+	display: flex;
+	justify-content: flex-start;
+	padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+	height: 90px;
+`;
+
+const MainCoverImageWrapper = styled.div`
+	text-align: center;
+`;
+
+const MainCover = styled.img`
+	height: 600px;
+`;
+
 
 export const Restaurants = () => {
 
@@ -13,7 +36,12 @@ export const Restaurants = () => {
 
 	return (
 		<>
-			レストラン一覧
+			<HeaderWrapper>
+				<MainLogoImage src={MainLogo} alt="main logo" />
+			</HeaderWrapper>
+			<MainCoverImageWrapper>
+				<MainCover src={MainCoverImage} alt="main cover" />
+			</MainCoverImageWrapper>
 		</>
 	)
 }
